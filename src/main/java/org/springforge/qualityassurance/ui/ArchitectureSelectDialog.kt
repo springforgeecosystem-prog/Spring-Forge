74fa8ff7bb9a63e70088243fd3df85a3650a4ebf
JameelaJabir <jamyjabir@gmail.com>
2025-12-29 15:42:55 +0530

    feat: add architecture selection dialog UI

--- src/main/java/org/springforge/qualityassurance/ui/ArchitectureSelectDialog.kt
+++ src/main/java/org/springforge/qualityassurance/ui/ArchitectureSelectDialog.kt
@@ -1,29 +1,29 @@
-//package org.springforge.qualityassurance.ui
-//
-//import com.intellij.openapi.ui.DialogWrapper
-//import javax.swing.JComboBox
-//import javax.swing.JComponent
-//import javax.swing.JLabel
-//import javax.swing.JPanel
-//
-//class ArchitectureSelectDialog : DialogWrapper(true) {
-//
-//    private lateinit var combo: JComboBox<String>
-//
-//    init {
-//        title = "Select Architecture Pattern"
-//        init()
-//    }
-//
-//    override fun createCenterPanel(): JComponent {
-//        val panel = JPanel()
-//        combo = JComboBox(arrayOf("Layered", "Hexagonal", "Clean", "MVC"))
-//        panel.add(JLabel("Architecture:"))
-//        panel.add(combo)
-//        return panel
-//    }
-//
-//    fun getSelectedArchitecture(): String {
-//        return combo.selectedItem as String
-//    }
-//}
+package org.springforge.qualityassurance.ui
+
+import com.intellij.openapi.ui.DialogWrapper
+import javax.swing.JComboBox
+import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+class ArchitectureSelectDialog : DialogWrapper(true) {
+
+    private lateinit var combo: JComboBox<String>
+
+    init {
+        title = "Select Architecture Pattern"
+        init()
+    }
+
+    override fun createCenterPanel(): JComponent {
+        val panel = JPanel()
+        combo = JComboBox(arrayOf("Layered", "Hexagonal", "Clean", "MVC"))
+        panel.add(JLabel("Architecture:"))
+        panel.add(combo)
+        return panel
+    }
+
+    fun getSelectedArchitecture(): String {
+        return combo.selectedItem as String
+    }
+}
