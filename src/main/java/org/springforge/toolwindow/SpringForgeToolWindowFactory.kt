8826982c036a91ffd94f1b5e34c9fd3751ed2013
IT22056320 <it22056320@my.sliit.lk>
2026-01-03 19:32:50 +0530

    Add SpringForge icons and integrate into tool window

--- src/main/java/org/springforge/toolwindow/SpringForgeToolWindowFactory.kt
+++ src/main/java/org/springforge/toolwindow/SpringForgeToolWindowFactory.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
+import org.springforge.icons.SpringForgeIcons
 
 /**
  * Main SpringForge Tool Window Factory
@@ -12,6 +13,9 @@ import com.intellij.ui.content.ContentFactory
 class SpringForgeToolWindowFactory : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        // Set custom icon
+        toolWindow.setIcon(SpringForgeIcons.ToolWindow)
+
         // Get or create the service
         val service = project.getService(SpringForgeToolWindowService::class.java)
         service.toolWindow = toolWindow
